40b7b8b8d966d401f1e9c53aa8518694db681768
HAXMINATOR <thor_e_jensen@hotmail.com>
2021-12-09 11:31:12 +0100

    Added basic MQ to ServerManagerServerInstanceMQ

---
+++ Server Manager/ServerManagerServerInstanceMQ.cs
@@ -0,0 +1,45 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server_Manager
+{
+    public class ServerManagerServerInstanceMQ
+    {
+        IConnection connection;
+        IModel channel;
+        EventingBasicConsumer consumer;
+        string queueName;
+
+        public ServerManagerServerInstanceMQ(string hostIPv4 = "localhost", string queueName = "server_manager_server_instance_queue")
+        {
+            this.queueName = queueName;
+
+            ConnectionFactory factory = new ConnectionFactory() { HostName = hostIPv4, UserName = "gruppe5", Password = "123" };
+            connection = factory.CreateConnection();
+            channel = connection.CreateModel();
+            channel.QueueDeclare(queueName, false, false, false, null);
+            channel.BasicConsume(queueName, false, consumer);
+
+            consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) =>
+            {
+                byte[] body = ea.Body.ToArray();
+                string msg = Encoding.UTF8.GetString(body);
+                //do something with msg
+            };
+
+            channel.BasicConsume(consumer, queueName, true);
+        }
+
+        public void Call(string message)
+        {
+            byte[] msg = Encoding.UTF8.GetBytes(message);
+            channel.BasicPublish("ServerManagerServerInstance", queueName, null, msg);
+        }
+    }
+}
